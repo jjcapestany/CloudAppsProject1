30806924ccf0cf87434ad3b03ca0dccc89a0e525
Justin Parry <justinsaiparry@hotmail.com>
2026-02-19 04:55:33 +0000

    update passwords in sample data to be human readable and hashed upon load

--- sample_data/load_data.py
+++ sample_data/load_data.py
@@ -3,6 +3,8 @@ import json
 from pathlib import Path
 import test_functions
 
+from api.auth.utils import hash_password
+
 def main():
     uri = "mongodb://localhost:27017/"
 
@@ -21,6 +23,8 @@ def main():
     hardware_collection_name = "hardware"
 
     doc_list = test_functions.read_json_file("sample_users.json")
+    for doc in doc_list:
+        doc['password_hash'] = hash_password(doc.pop('password_human_readable', None)) 
     test_functions.load_data(uri, users_db_name, users_collection_name, doc_list)
 
     doc_list = test_functions.read_json_file("sample_projects.json")
